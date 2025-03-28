42ecb4ae12ded05d1b445c65fdfe5b083f270e88
Cornelius Chebet <cornelius.chebet@student.moringaschool.com>
2025-03-29 02:47:36 +0300

    server.js update

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
+server.use(middlewares);
 
-const server = jsonServer.create()
-
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use('/api', router)
+// Enable CORS for all requests
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://njmwas.github.io') // The URL you put here is for the web application that you have deployed using Github Pages
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-})
-server.listen(process.env.PORT || 5000, () => {
-    console.log('JSON Server is running')
-})
+    res.header('Access-Control-Allow-Origin', '*'); // Allows requests from all domains
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS'); // Allows necessary HTTP methods
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204); // Preflight request response
+    }
+    
+    next();
+});
+
+server.use(router); // Removes the `/api` prefix
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
+});
